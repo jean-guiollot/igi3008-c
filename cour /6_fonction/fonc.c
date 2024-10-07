ba5763a9722cc4f1996303fe89fd635c6798e1de
jean-guiollot <jean.guiollot@edu.esiee.fr>
2024-10-07 14:40:02 +0000

    coucou

---
+++ cour /6_fonction/fonc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int sumult(int a ,int b,int *somme,int *prod)
+{
+    *somme = a+b;
+    *prod = a*b;
+    return 0;
+}
+
+
+
+
+int main()
+{
+    int a,b,somme,prod;
+    a=3;
+    b=4;
+    sumult(a,b,&somme,&prod);
+    printf("test fonction 3+4 = %d\n 3*4=%d\n",somme,prod);
+    return 0;
+}
+
+
+
+
+
+
+
