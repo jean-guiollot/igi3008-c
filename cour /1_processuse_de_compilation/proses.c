e9c97b5ec68f5096ef3716231e9c9ade1ad90732
jean-guiollot <jean.guiollot@edu.esiee.fr>
2024-10-07 14:50:08 +0000

    coucou

--- cour /1_processuse_de_compilation/proses.c
+++ cour /1_processuse_de_compilation/proses.c
@@ -2,3 +2,11 @@ int main(){
     printf("Hello World !");
     return 0;
 }
+//executer les commende 
+//    gcc -Wall fonc.c -o nb -lm
+//    ./nb
+
+
+//save les fichiers
+//   git add .
+//   git commit -m "coucou"
